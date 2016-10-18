54cda60d83c458131cb9881561337b91c0adb44c
Adam Zinder and Chris Wingo <chris@>
2016-10-18 15:51:17 -0700

    Finally add toggle functionality

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $(".initial-show").click(function(){
+    $('h1',this).toggle();
+    $('p', this).toggle();
+//    $(".reveal").toggle();
+//    $(".initial-show").toggle();
+//    $(.initial-show#(passed_id)).toggle();
+//    $(.initial-show#(passed_id)) p.toggle();
+  });
+});
